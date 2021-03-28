acb656d794d6fedc9024c71e991208a2466265c4
unknown <rajibkhan976@gmail.com>
2021-03-28 17:32:13 +0200

    image animation with css @keyframes scene4 done

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ document.onreadystatechange = function () {
 	var sceneOneImg = document.querySelector(".scene-one-bg-img");
 	var sceneTwo = document.querySelector(".scene-two-text-content");
 	var sceneTwoImg = document.querySelector(".scene-two-bg-img");
+	var sceneThree = document.querySelector(".scene-three-text-content");
+	var sceneThreeImg = document.querySelector(".scene-three-bg-img");
+	var sceneThreeBottom = document.querySelector(".scene-three-bottom-content");
+	var sceneFour = document.querySelector(".scene-four-text-content");
+	var sceneFourImg = document.querySelector(".scene-four-bg-img");
 	document.addEventListener("scroll", function (e) {
-		if (window.pageYOffset  <= 650) {
+		if (window.pageYOffset  >= 0 && window.pageYOffset  <= 650) {
 			sceneOne.classList.remove(`scene-one-text-content`);
 			sceneOne.classList.add(`scene-one-text-animation`);
 			sceneOneImg.classList.remove(`scene-one-bg-img`);
@@ -18,8 +23,24 @@ document.onreadystatechange = function () {
 			sceneTwoImg.classList.add(`scene-two-bgimg-animation`);
 			sceneTwo.classList.add(`scene-two-text-animation`);
 		} else {
-			sceneTwo.classList.remove(`scene-two-text-animation`);
 			sceneTwoImg.classList.remove(`scene-two-bgimg-animation`);
+			sceneTwo.classList.remove(`scene-two-text-animation`);
+		}
+		if (window.pageYOffset  >= 1301 && window.pageYOffset  <= 1950) {
+			sceneThree.classList.add(`scene-three-text-animation`);
+			sceneThreeImg.classList.add(`scene-three-bgimg-animation`);
+			sceneThreeBottom.classList.add(`scene-three-bottom-animation`);
+		} else {
+			sceneThree.classList.remove(`scene-three-text-animation`);
+			sceneThreeImg.classList.remove(`scene-three-bgimg-animation`);
+			sceneThreeBottom.classList.remove(`scene-three-bottom-animation`);
+		}
+		if (window.pageYOffset  >= 1951 && window.pageYOffset  <= 2850) {
+			sceneFour.classList.add(`scene-four-text-animation`);
+			sceneFourImg.classList.add(`scene-four-bgimg-animation`);
+		} else {
+			sceneFour.classList.remove(`scene-four-text-animation`);
+			sceneFourImg.classList.remove(`scene-four-bgimg-animation`);
 		}
 	});
   }
